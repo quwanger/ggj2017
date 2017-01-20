3155aa300c3874e8e372f99f72c52b40b05e043e
Andrew Brough <Andrew Brough>
2017-01-20 18:33:08 -0500

    Initial commit with XBOX controller working with character controller.

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    public float moveSpeed = 1;
+    public Vector2 maxVelocity;
+
+    public float deadzoneX = 0.01f;
+    public float deadzoneY = 0.01f;
+
+    private float joyX = 0.0f;
+    private float joyY = 0.0f;
+
+    //camera
+    public Camera playerCamera;
+    public float cameraMoveSpeed;
+
+    // Use this for initialization
+    void Start () {
+        playerCamera = FindObjectOfType<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        joyX = Input.GetAxisRaw("Horizontal");
+        joyY = Input.GetAxis("Vertical");
+        //Debug.Log("Horizontal Input: " + joyX);
+        //Debug.Log("Vertical Input: " + joyY);
+
+        if (Mathf.Abs(joyX) > deadzoneX && GetComponent<Rigidbody2D>().velocity.x < maxVelocity.x) {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(moveSpeed * joyX, 0.0f));
+        } if(Mathf.Abs(joyY) > deadzoneY && GetComponent<Rigidbody2D>().velocity.y < maxVelocity.y) {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, moveSpeed * joyY));
+        }
+
+
+        Vector3 diff = transform.position + new Vector3(joyX, joyY, 0) - transform.position;
+        diff.Normalize();
+        if(diff.magnitude > 0.01f) {
+            float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);
+        }
+
+        float camDistance = Vector2.Distance(transform.position, playerCamera.transform.position);
+        if ( camDistance > 0.1f) {
+            Vector2 targetCamDirection = transform.position - playerCamera.transform.position;
+            playerCamera.GetComponent<Rigidbody2D>().AddForce(targetCamDirection.normalized * cameraMoveSpeed * camDistance);
+        }
+	}
+}
