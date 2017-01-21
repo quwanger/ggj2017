e172c2af58ec9c3eca411734231aad783ef09098
Andrew Brough <Andrew Brough>
2017-01-21 13:00:16 -0500

    Working aiming words in an arc

--- Assets/Scripts/LetterProjectile.cs
+++ Assets/Scripts/LetterProjectile.cs
@@ -2,13 +2,15 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class Projectile : MonoBehaviour {
+public class LetterProjectile : MonoBehaviour {
 
     public Text text;
     public int damage = 1;
 
     public float life = 0.0f;
     public float timeout = 5.0f;
+
+    public int bounces;
 
 	// Use this for initialization
 	void Start () {
@@ -18,16 +20,22 @@ public class Projectile : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         life += Time.deltaTime;
-        if(life >= timeout) {
+        if(life >= timeout || bounces <= 0) {
             Destroy(gameObject);
         }
 	}
 
     public void OnCollisionEnter2D(Collision2D collision) {
+        
+        
         CharacterController cc = collision.collider.GetComponent<CharacterController>();
         if(cc != null) {
-            Debug.Log("hit!");
+            Debug.Log("hit a player!");
             cc.Damage(damage);
+        }
+
+        if(collision.collider.GetComponent<LetterProjectile>() == null) {
+            bounces--;
         }
     }
 }
---
+++ Assets/Scripts/Word.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class Word {
+
+    public List<LetterProjectile> letters;
+    public string word;
+
+    public Word(string word) {
+        this.word = word;
+        letters = new List<LetterProjectile>();
+    }
+
+    public void Fire(float speed) {
+        
+        foreach(LetterProjectile letter in letters) {
+            letter.GetComponent<Collider2D>().enabled = true;
+            letter.GetComponent<Rigidbody2D>().AddForce(letter.transform.up * speed);
+            Vector3 pos = letter.transform.position;
+            letter.transform.parent = null;
+            letter.transform.position = pos;
+        }
+    }
+}
