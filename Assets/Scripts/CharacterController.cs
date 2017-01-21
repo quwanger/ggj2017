c7e222799b0e1f9705787d679fab3e6ae422b2f6
Andrew Brough <Andrew Brough>
2017-01-20 22:35:04 -0500

    Added projectiles, shooting

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class CharacterController : MonoBehaviour {
 
     public int playerIndex;
     public float moveSpeed = 100;
+    public int health = 1;
 
     private Vector2 leftJoystick = Vector2.zero;
     private Vector2 rightJoystick = Vector2.zero;
@@ -18,10 +20,11 @@ public class CharacterController : MonoBehaviour {
 	void Update () {
         Move();
         Look();
-	}
+        Fire();
+    }
 
     private void Move() {
-        leftJoystick.x = Input.GetAxisRaw("Player"+playerIndex+"MoveX");
+        leftJoystick.x = Input.GetAxis("Player"+playerIndex+"MoveX");
         leftJoystick.y = Input.GetAxis("Player"+playerIndex+"MoveY");
 
         GetComponent<Rigidbody2D>().AddForce(new Vector2(moveSpeed * leftJoystick.x, 0.0f));
@@ -29,30 +32,47 @@ public class CharacterController : MonoBehaviour {
     }
 
     private void Look() {
-        rightJoystick.x = Input.GetAxisRaw("Player" + playerIndex + "LookX");
+        rightJoystick.x = Input.GetAxis("Player" + playerIndex + "LookX");
         rightJoystick.y = -Input.GetAxis("Player" + playerIndex + "LookY");
         Vector3 diff = transform.position + new Vector3(rightJoystick.x, rightJoystick.y, 0) - transform.position;
         diff.Normalize();
         float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);
 
-        Fire();
+        Debug.DrawRay(transform.position, transform.up * -10, Color.red);
 	}
+
+    bool charging = false;
+    public float projectileSpeed = 100.0f;
 
     void Fire()
     {
-        bool charging = false;
-
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetButton("Player"+playerIndex+"Fire1"))
         {
             charging = true;
-
             GetComponent<Animator>().SetTrigger("Firing");
-        } else
+        } else if(Input.GetButtonUp("Player"+playerIndex+"Fire1"))
         {
+            SpawnProjectile("F");
             charging = false;
-
             //GetComponent<Animator>().ResetTrigger("Firing");
+        }
+    }
+
+    private void SpawnProjectile(string text) {
+        Projectile projectile = (Projectile)Instantiate(Resources.Load<Projectile>("Letter Projectile"), transform.position + transform.up * -1.5f, Quaternion.identity);
+        projectile.text.text = text;
+        Vector3 diff = transform.up * -1;
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        projectile.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+        projectile.GetComponent<Rigidbody2D>().AddForce(transform.up * -projectileSpeed);
+    }
+
+    public void Damage(int damage) {
+        health -= damage;
+        if (health <= 0) {
+            //other player wins!
+            Destroy(gameObject);
         }
     }
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Projectile : MonoBehaviour {
+
+    public Text text;
+    public int damage = 1;
+
+    public float life = 0.0f;
+    public float timeout = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        life += Time.deltaTime;
+        if(life >= timeout) {
+            Destroy(gameObject);
+        }
+	}
+
+    public void OnCollisionEnter2D(Collision2D collision) {
+        CharacterController cc = collision.collider.GetComponent<CharacterController>();
+        if(cc != null) {
+            Debug.Log("hit!");
+            cc.Damage(damage);
+        }
+    }
+}
