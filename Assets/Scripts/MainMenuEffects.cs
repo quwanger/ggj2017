dc7c2b9ccc1faba5aaee4449c48d43a5f98c4ac2
Andrew Brough <Andrew Brough>
2017-01-22 21:01:08 -0500

    Fixed UI scaling

--- Assets/Scripts/MainMenuEffects.cs
+++ Assets/Scripts/MainMenuEffects.cs
@@ -4,35 +4,17 @@ using UnityEngine.EventSystems;
 
 public class MainMenuEffects : MonoBehaviour {
 
-    public GameObject playButton;
-    public GameObject optionsButton;
-    public GameObject exitButton;
-    public GameObject canvas;
-    public int moveSpeed = 2000;
-
     // Use this for initialization
     void Start () {
-        //playButton.transform.Translate(-1000, 0, 0);
-        //optionsButton.transform.Translate(1000, 0, 0);
-        //exitButton.transform.Translate(-1000, 0, 0);
+
     }
 
 	// Update is called once per frame
 	void Update () {
-        //if (playButton.transform.position.x - canvas.transform.position.x < 0) {
-        //    playButton.transform.Translate(Vector3.right * 2000 * Time.deltaTime);
-        //}
-        //else {
-        //    if(optionsButton.transform.position.x - canvas.transform.position.x > 0) {
-        //        optionsButton.transform.Translate(Vector3.right * -2000 * Time.deltaTime);
-        //    }
-        //    else {
-        //        if (exitButton.transform.position.x - canvas.transform.position.x < 0){
-        //            exitButton.transform.Translate(Vector3.right * 2000 * Time.deltaTime);
-        //        }
+        //for(int i = 1; i < 12; i++) {
+        //    if (Input.GetButton("Player"+i+"Fire1")) {
+        //        Debug.Log(i);
         //    }
         //}
-        
-               
 	}
 }
